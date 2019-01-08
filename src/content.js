2d4cfd17c38dfded8a1845ab8752c288e6cb47da
Marco Fugaro <marco.fugaro@gmail.com>
2019-01-08 14:34:18 +0100

    💄 Prettier the code

--- src/content.js
+++ src/content.js
@@ -7,41 +7,44 @@ function replaceNpmUrls(resultContainer) {
   const npmPackageUrl = 'https://www.npmjs.com/package/'
   const npmLinks = [...resultContainer.querySelectorAll(`a[href^="${npmPackageUrl}"]`)]
 
-  npmLinks.forEach(async (el) => {
-    const npmUrl = el.getAttribute('href')
+  npmLinks.forEach(async link => {
+    const npmUrl = link.getAttribute('href')
     const packageName = npmUrl.slice(npmPackageUrl.length)
 
     const githubUrl = await getPackageGithubUrl(packageName)
-    if (!githubUrl)
+    if (!githubUrl) {
       return
+    }
 
-    el.setAttribute('href', githubUrl)
-    const title = el.querySelector('h3') || el
+    link.setAttribute('href', githubUrl)
+    const title = link.querySelector('h3') || link
     title.textContent = title.textContent.replace(' - npm', ' - GitHub')
 
-
-    const npmUrlNodes = [...resultContainer.querySelectorAll('cite')].filter((el) => el.textContent === npmUrl)
-    npmUrlNodes.forEach((el) => { el.textContent = githubUrl })
+    const npmUrlNodes = [...resultContainer.querySelectorAll('cite')].filter(
+      el => el.textContent === npmUrl,
+    )
+    npmUrlNodes.forEach(el => {
+      el.textContent = githubUrl
+    })
 
     // re-update the link each time google plays with it and changes it
     // this is done to prevent the "Redirect notice" when you open the link in a new tab
     const hrefObserver = new MutationObserver(() => {
-      if (el.getAttribute('href') !== githubUrl) {
-        el.setAttribute('href', githubUrl)
+      if (link.getAttribute('href') !== githubUrl) {
+        link.setAttribute('href', githubUrl)
       }
     })
-    hrefObserver.observe(el, {
+    hrefObserver.observe(link, {
       attributes: true,
       attributeFilter: ['href'],
     })
   })
 }
 
-
 async function init() {
   // if we start from the main google page, there isn't yet a #search div
   // so we wait for it
-  const resultContainer = document.getElementById('search') || await pGetElementById('search')
+  const resultContainer = document.getElementById('search') || (await pGetElementById('search'))
 
   // call it a first time
   replaceNpmUrls(resultContainer)
